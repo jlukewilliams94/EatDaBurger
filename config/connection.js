cb96f3aaa95a7ac635964f94169e813f3bc2f5e1
luke williams <jluke94williams@gmail.com>
2020-04-20 19:11:16 -0400

    Another bug fix

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,6 @@ if(process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL); 
 } else {connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
   user: "root",
   password: "abcd123!",
   database: "burger_db"
