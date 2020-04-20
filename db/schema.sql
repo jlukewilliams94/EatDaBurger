81a16700ce9f2c862f2f788c1d2952ba696dc0b6
luke williams <jluke94williams@gmail.com>
2020-04-20 12:10:19 -0400

    Added content to db folder

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE burger_db;
+USE burger_db;
+
+CREATE TABLE burger
+(
+	id INT NOT NULL AUTO_INCREMENT,
+	burger_name VARCHAR(255) NOT NULL,
+	devoured BOOLEAN,
+	PRIMARY KEY (id)
+);
