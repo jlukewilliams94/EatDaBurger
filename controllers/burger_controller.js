695def8cf3a8d4c4355aae33f16580d43a5f9693
luke williams <jluke94williams@gmail.com>
2020-04-20 13:14:24 -0400

    Established basic api routes

---
+++ controllers/burger_controller.js
@@ -0,0 +1,29 @@
+const express = require("express");
+
+const router = express.Router();
+
+let burgers = require("../models/burger");
+
+// Get current burgers
+router.get("/", funciton(req,res){
+
+
+});
+
+//Create new burger
+router.post("/api/burgers", function(req,res)[
+
+
+]);
+
+//Update Burger
+router.put("/api/burgers/:id", function(req, res){
+
+
+});
+
+//Delet Burger
+
+router.delete("/api/burgers/:id", funciton(req, res){
+    
+})
