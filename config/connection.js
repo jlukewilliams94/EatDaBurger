79ee035cf5d2d3870ebf244cc22085ae190a1da0
luke williams <jluke94williams@gmail.com>
2020-04-20 19:01:00 -0400

    bug issues resolved

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,18 @@
 const mysql = require("mysql");
+let connection;
 
 if(process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL); 
 } else {connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
   user: "root",
   password: "Mustangs!",
   database: "burger_db"
-});}
+});
+};
 
   
-  // Make connection.
+// Make connection.
   connection.connect(function(err) {
     if (err) {
       console.error("error connecting: " + err.stack);
@@ -21,5 +22,5 @@ if(process.env.JAWSDB_URL){
   });
   
   // Export connection for our ORM to use.
-  module.exports = connection;
+module.exports = connection;
   
