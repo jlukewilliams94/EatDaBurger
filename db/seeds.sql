262f8902a6406632fea1888d48b1dafc5a04684c
luke williams <jluke94williams@gmail.com>
2020-04-20 12:25:34 -0400

    Addec content to schema and seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (devoured, burger_name) VALUES (true, 'Arnold Ham and Egger');
+INSERT INTO burgers (devoured, burger_name) VALUES (true, 'Texas Longhorn');
+INSERT INTO burgers (devoured, burger_name) VALUES (false, 'British Burger');
