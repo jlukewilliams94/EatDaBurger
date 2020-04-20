c56f08d68b1d8ac9281b0d814ac32517bb66608a
luke williams <jluke94williams@gmail.com>
2020-04-20 17:22:09 -0400

    Built dabuger.js call backs

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -19,13 +19,20 @@ router.get("/", function(req,res){
 
 //Create new burger
 router.post("/api/burgers", function(req,res){
-
-
+    burgers.create([
+        "burger_name", "devoured"
+      ], [
+        req.body.burger_name, req.body.devoured
+      ], function(result) {
+        // Send back the ID of the new quote
+        res.json({ id: result.insertId });
+      });
+   
 });
 
 //Update Burger
 router.put("/api/burgers/:id", function(req, res){
-
+    
 
 });
 
---
+++ public/assets/js/daBurger.js
@@ -0,0 +1,53 @@
+
+
+$(function(){
+    // Devoured Button Function
+    $(".eat-burger").on("click", function (event){
+        event.preventDefault();
+
+        let burgerId = $(this).data("id");
+        let ifDevoured = $(this).data("ifdevoured");
+
+        let ifEaten = {
+            devoured = ifDevoured
+        };
+        $.ajax("/api/burgers/" + burgerId, {
+            type: "PUT",
+            data: ifEaten
+        }).then(function(){
+            location.reload();
+            console.log("More Please")
+        })
+    });
+    //Delete a Burger
+    $(".remove-burger").on("click", function(event){
+        event.preventDefault();
+        let burgerId = $(this).data("id");
+        $.ajax("/api/burgers/" + burgerId, {
+            type: "DELETE"
+        }).then(function(){
+            location.reload();
+            console.log("Bye Bye Burger")
+        })
+    });
+
+    //Create a Burger
+    $(".burger-form").on("submit", function(event){
+        event.preventDefault();
+        let createdBurger = {
+            burger_name: $("#daNewBurger").val().trim(),
+            devoured: 0
+        }
+        console.log(createdBurger)
+        $.ajax("/api/burgers", {
+            type: "POST", 
+            data: createdBurger
+        }).then(function(){
+            location.reload();
+            console.log("Ya got a new buger to eat")
+
+        })
+    });
+
+
+});
